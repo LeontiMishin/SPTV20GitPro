f85b7a78e47c8d0116b22c8da528843cccd930eb
Your Name <you@example.com>
2021-09-16 14:33:44 +0300

    vtaraja versija

--- src/sptv20gitpro/SPTV20GitPro.java
+++ src/sptv20gitpro/SPTV20GitPro.java
@@ -16,6 +16,7 @@ public class SPTV20GitPro {
      */
     public static void main(String[] args) {
         System.out.println("Novaja versija projekta");
+        System.out.println("Vtaraja versija projekta");
     }
     
 }
